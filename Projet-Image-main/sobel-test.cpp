207bf131c0e6588a7f785046e6cfa365c0da473f
XVR477 <145608915+XVR477@users.noreply.github.com>
2025-12-02 12:08:39 +0100

    cado
    prout

--- Projet-Image-main/sobel-test.cpp
+++ Projet-Image-main/sobel-test.cpp
@@ -9,13 +9,18 @@ using namespace std;
 #include "sobel.hpp"
 #include "image-test.hpp"
 
-void sobelTest() {
-    // Remplacez cette ligne et la suivante par le code adéquat
-    throw runtime_error("Fonction sobelTest non implantée ligne 14");
+void testSobel(){
+    cout << "Vérifier que les images obtenues dans 'sobel/' sont semblables à celles fournies dans 'sobel/correction/'" << endl;
+    ecrirePGM(intensite(lirePGM("images/Willis.512.pgm" )), "sobel/Willis.512.pgm");
+    ecrirePGM(intensite(lirePGM("images/Baboon.512.pgm" )), "sobel/Baboon.512.pgm");
+    ecrirePGM(intensite(lirePGM("images/Billes.256.pgm" )), "sobel/Billes.256.pgm");
+    ecrirePGM(intensite(lirePGM("images/Embryos.512.pgm")), "sobel/Embryos.512.pgm");
+    ecrirePGM(intensite(lirePGM("images/House.256.pgm"  )), "sobel/House.256.pgm");
 }
+
 
 int main(){
-    // Remplacez cette ligne et la suivante par le code adéquat
-    throw runtime_error("Fonction main non implantée ligne 19");
-}
 
+    testSobel();
+    return 0 ;
+}
