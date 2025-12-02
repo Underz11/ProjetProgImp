7683cd034d7ea7947ee7a05316b421a20b9f412c
XVR477 <145608915+XVR477@users.noreply.github.com>
2025-12-02 12:09:23 +0100

    ez
    ez

--- Projet-Image-main/sobel.cpp
+++ Projet-Image-main/sobel.cpp
@@ -6,17 +6,36 @@
 #include "sobel.hpp"
 
 ImageGris intensiteH(ImageGris img) {
-    // Remplacez cette ligne et la suivante par le code adéquat
-    throw runtime_error("Fonction intensiteH non implantée ligne 10");
+    auto nligne = vector<double> ( img[0].size() );
+    for (int a = 0 ; a < nligne.size() ; a++) nligne[a] = 0;
+    auto nimg = ImageGris ( img.size() );
+    for ( int b = 0; b < nimg.size() ; b++) nimg[b] = nligne;
+    for( int i = 1 ; i < img.size()-1 ; i++ ) for ( int j = 0 ; j < img[i].size()-1 ; j++)
+        if (j!=0) nimg[i][j] = img[i-1][j-1] + 2*img[i][j-1] +
+            img[i+1][j-1]- img[i-1][j+1] - 2*img[i][j+1] - img[i+1][j+1];
+    return nimg;
 }
 
 ImageGris intensiteV(ImageGris img) {
-    // Remplacez cette ligne et la suivante par le code adéquat
-    throw runtime_error("Fonction intensiteV non implantée ligne 15");
+    auto nligne = vector<double> ( img[0].size() );
+    for (int a = 0 ; a < nligne.size() ; a++) nligne[a] = 0;
+    auto nimg = ImageGris ( img.size() );
+    for ( int b = 0; b < nimg.size() ; b++) nimg[b] = nligne;
+    for( int i = 1 ; i < img.size()-1 ; i++ ) for ( int j = 0 ; j < img[i].size()-1 ; j++)
+        if (j!=0) nimg[i][j] = img[i-1][j-1] + 2*img[i-1][j] + img[i-1][j+1] - img[i+1][j-1] - 2*img[i+1][j] - img[i+1][j+1];
+    return nimg;
 }
 
 ImageGris intensite(ImageGris img) {
-    // Remplacez cette ligne et la suivante par le code adéquat
-    throw runtime_error("Fonction intensite non implantée ligne 20");
+    auto nligne = vector<double> ( img[0].size() );
+    for (int a = 0 ; a < nligne.size() ; a++) nligne[a] = 0;
+    auto nimg = ImageGris ( img.size() );
+    for ( int b = 0; b < nimg.size() ; b++) nimg[b] = nligne;
+    for( int i = 1 ; i < img.size()-1 ; i++ ) for ( int j = 0 ; j < img[i].size()-1 ; j++)
+        if (j!=0){
+            double h = img[i-1][j-1] + 2*img[i][j-1] + img[i+1][j-1]- img[i-1][j+1] - 2*img[i][j+1] - img[i+1][j+1];
+            double v = img[i-1][j-1] + 2*img[i-1][j] + img[i-1][j+1] - img[i+1][j-1] - 2*img[i+1][j] - img[i+1][j+1];
+            nimg[i][j] = sqrt( h*h + v*v );
+        }
+    return nimg;
 }
-
